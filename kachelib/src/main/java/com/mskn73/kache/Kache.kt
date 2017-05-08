4a83df316d3834454f1ccba92dcd6ba50f0533f8
Francisco Hernandez <francisco.hernandez.87@gmail.com>
2017-05-08 13:29:07 +0200

    sync Put y get functions added in cache

---
+++ kachelib/src/main/java/com/mskn73/kache/Kache.kt
@@ -0,0 +1,45 @@
+package com.mskn73.kache
+
+import android.content.Context
+import com.mskn73.kache.io.FileManager
+import com.mskn73.kache.io.Serializer
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.CoroutineStart
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.runBlocking
+import java.io.File
+import kotlin.system.measureTimeMillis
+
+/**
+ * Created by francisco.hernandez on 8/5/17.
+ */
+
+class Kache(val context: Context, val cacheDir: String) {
+  private val SETTINGS_FILE_NAME = "com.mskn73.kache.SETTINGS"
+
+  val fileManager: FileManager = FileManager()
+  val serializer: Serializer = Serializer()
+
+
+  fun put(myKacheable: Kacheable) {
+    val file = buildFileForKey(myKacheable.key)
+    val serializedObject = serializer.serialize(myKacheable, myKacheable.javaClass)
+    fileManager.writeToFile(file, serializedObject)
+    setLastCacheUpdate(myKacheable.key)
+  }
+
+  operator fun get(clazz: Class<*>, key: String): Any? {
+    val file = buildFileForKey(key)
+    val fileContent = fileManager.readFileContent(file)
+    val obj = serializer.deserialize(fileContent, clazz)
+    return obj
+  }
+
+  private fun setLastCacheUpdate(key: String) {
+    fileManager.writeToPreferences(context, SETTINGS_FILE_NAME, key, System.currentTimeMillis())
+  }
+
+  private fun buildFileForKey(key: String): File {
+    return File(cacheDir + File.separator + key)
+  }
+}
